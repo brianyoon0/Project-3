4f09f7a468346a32365c7d0a533d59ca6dd3e415
brian <brianyoon2006@gmail.com>
2025-12-02 15:12:31 -0500

    Set the header file to have the different Structs needed as well as a graph for the Campus Compass

--- src/CampusCompass.h
+++ src/CampusCompass.h
@@ -1,12 +1,53 @@
 #pragma once
 #include <string>
-
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
+
+struct Edge {
+    int to;
+    int time;
+    bool open;
+};
+
+struct ClassInfo {
+    int locationID;
+    int startMinutes;
+    int endMinutes;
+};
+
+struct Student {
+    string name;
+    string ufidl;
+    int residenceLocation;
+    vector<string> classCodes;
+};
+
+class Graph {
+public:
+    void addEdge(int u, int v, int time);
+    void toggleEdge(int u, int v);
+    string getEdgeStatus(int u, int v) const;
+    bool isConnected(int start, int goal) const;
+
+    unordered_map<int, int> dijkstra(int src) const;
+
+private:
+    unordered_map<int, vector<Edge>> adj;
+};
+
 
 class CampusCompass {
 private:
-    // Think about what member variables you need to initialize
-    // perhaps some graph representation?
+    Graph graph;
+    unordered_map<string, ClassInfo> classByCode;
+    unordered_map<string, Student> studentByID;
+    //CSV loading helpers
+    bool loadEdges(const string &edges_filepath);
+    bool loadClasses(const string &classes_filepath);
+    int parseTimetoMinutes(const string &timesStr) const;
+
 public:
     // Think about what helper functions you will need in the algorithm
     CampusCompass(); // constructor
